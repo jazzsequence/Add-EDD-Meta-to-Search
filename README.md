abadc61aded2aba138fff9212e3fbc8d00533aaa
Chris Reynolds <chris@jazzsequence.com>
2016-10-25 08:26:32 -0600

    initial commit

---
+++ README.md
@@ -0,0 +1,36 @@
+# WDS Pluginize Add EDD Meta to Search #
+**Contributors:**      WebDevStudios  
+**Donate link:**       http://webdevstudios.com  
+**Tags:**  
+**Requires at least:** 4.4  
+**Tested up to:**      4.6.1 
+**Stable tag:**        1.0.0  
+**License:**           GPLv2  
+**License URI:**       http://www.gnu.org/licenses/gpl-2.0.html  
+
+## Description ##
+
+Adds Easy Digital Downloads meta and taxonomies to WordPress search.
+
+## Installation ##
+
+### Manual Installation ###
+
+1. Upload the entire `/wds-pluginize-edd-search` directory to the `/wp-content/plugins/` directory.
+2. Activate WDS Pluginize Add EDD Meta to Search through the 'Plugins' menu in WordPress.
+
+## Frequently Asked Questions ##
+
+
+## Screenshots ##
+
+
+## Changelog ##
+
+### 0.1.0 ###
+* First release
+
+## Upgrade Notice ##
+
+### 0.1.0 ###
+First Release
---
+++ add-edd-meta-to-search.php
@@ -0,0 +1,241 @@
+<?php
+/**
+ * Plugin Name: Add EDD Meta to Search
+ * Plugin URI:  http://webdevstudios.com
+ * Description: Adds Easy Digital Downloads meta and taxonomies to WordPress search.
+ * Version:     1.0.0
+ * Author:      WebDevStudios
+ * Author URI:  http://webdevstudios.com
+ * Donate link: http://webdevstudios.com
+ * License:     GPLv2
+ * Text Domain: wds-pluginize-edd-search
+ * Domain Path: /languages
+ *
+ * @link http://webdevstudios.com
+ *
+ * @package Add EDD Meta to Search
+ * @version 1.0.0
+ */
+
+/**
+ * Copyright (c) 2016 WebDevStudios (email : contact@webdevstudios.com)
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License, version 2 or, at
+ * your discretion, any later version, as published by the Free
+ * Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+/**
+ * Main initiation class
+ *
+ * @since  1.0.0
+ */
+final class WDS_EDD_Search {
+
+	/**
+	 * Current version
+	 *
+	 * @var  string
+	 * @since  1.0.0
+	 */
+	const VERSION = '1.0.0';
+
+	/**
+	 * Plugin basename
+	 *
+	 * @var string
+	 * @since  1.0.0
+	 */
+	protected $basename = '';
+
+	/**
+	 * Singleton instance of plugin
+	 *
+	 * @var WDS_EDD_Search
+	 * @since  1.0.0
+	 */
+	protected static $single_instance = null;
+
+	/**
+	 * Creates or returns an instance of this class.
+	 *
+	 * @since  1.0.0
+	 * @return WDS_EDD_Search A single instance of this class.
+	 */
+	public static function get_instance() {
+		if ( null === self::$single_instance ) {
+			self::$single_instance = new self();
+		}
+
+		return self::$single_instance;
+	}
+
+	/**
+	 * Sets up our plugin
+	 *
+	 * @since  1.0.0
+	 */
+	protected function __construct() {
+		$this->basename = plugin_basename( __FILE__ );
+	}
+
+	/**
+	 * Add hooks and filters
+	 *
+	 * @since  1.0.0
+	 * @return void
+	 */
+	public function hooks() {
+		add_action( 'init', array( $this, 'init' ) );
+		add_filter( 'posts_where', array( $this, 'add_to_search' ) );
+	}
+
+	/**
+	 * Init hooks
+	 *
+	 * @since  1.0.0
+	 * @return void
+	 */
+	public function init() {
+		if ( $this->check_requirements() ) {
+			load_plugin_textdomain( 'wds-pluginize-edd-search', false, dirname( $this->basename ) . '/languages/' );
+			$this->plugin_classes();
+		}
+	}
+
+	/**
+	 * Check if the plugin meets requirements and
+	 * disable it if they are not present.
+	 *
+	 * @since  1.0.0
+	 * @return boolean result of meets_requirements
+	 */
+	public function check_requirements() {
+		if ( ! $this->meets_requirements() ) {
+
+			// Add a dashboard notice.
+			add_action( 'all_admin_notices', array( $this, 'requirements_not_met_notice' ) );
+
+			// Deactivate our plugin.
+			add_action( 'admin_init', array( $this, 'deactivate_me' ) );
+
+			return false;
+		}
+
+		return true;
+	}
+
+	/**
+	 * Deactivates this plugin, hook this function on admin_init.
+	 *
+	 * @since  1.0.0
+	 * @return void
+	 */
+	public function deactivate_me() {
+
+		// We do a check for deactivate_plugins before calling it, to protect
+		// any developers from accidentally calling it too early and breaking things.
+		if ( function_exists( 'deactivate_plugins' ) ) {
+			deactivate_plugins( $this->basename );
+		}
+	}
+
+	/**
+	 * Check that all plugin requirements are met
+	 *
+	 * @since  1.0.0
+	 * @return boolean True if requirements are met.
+	 */
+	public function meets_requirements() {
+		// Do checks for required classes / functions
+		if ( ! class_exists( 'Easy_Digital_Downloads' ) ) {
+			return false;
+		}
+
+		// We have met all requirements.
+		return true;
+	}
+
+	/**
+	 * Adds a notice to the dashboard if the plugin requirements are not met
+	 *
+	 * @since  1.0.0
+	 * @return void
+	 */
+	public function requirements_not_met_notice() {
+		// Output our error.
+		echo '<div id="message" class="error">';
+		echo '<p>' . sprintf( __( 'Add EDD Meta to Search is missing requirements and has been <a href="%s">deactivated</a>. Please make sure all requirements are available.', 'wds-pluginize-edd-search' ), admin_url( 'plugins.php' ) ) . '</p>';
+		echo '</div>';
+	}
+
+	/**
+	 * Magic getter for our object.
+	 *
+	 * @since  1.0.0
+	 * @param string $field Field to get.
+	 * @throws Exception Throws an exception if the field is invalid.
+	 * @return mixed
+	 */
+	public function __get( $field ) {
+		switch ( $field ) {
+			case 'version':
+				return self::VERSION;
+			case 'basename':
+				return $this->$field;
+			default:
+				throw new Exception( 'Invalid ' . __CLASS__ . ' property: ' . $field );
+		}
+	}
+
+	/**
+	 * Include a file from the includes directory
+	 *
+	 * @since  1.0.0
+	 * @param  string $filename Name of the file to be included.
+	 * @return bool   Result of include call.
+	 */
+	public static function include_file( $filename ) {
+		$file = self::dir( $filename . '.php' );
+		if ( file_exists( $file ) ) {
+			return include_once( $file );
+		}
+		return false;
+	}
+
+
+	public function add_to_search( $where ) {
+		// Load the meta keys into an array.
+		$keys = array( 'edd_download_files' ); // Only using one but we can easily add more meta keys here if we need to.
+
+		if ( is_search() && ! is_admin() ) {
+			global $wpdb;
+
+			$query = get_search_query();
+			$query = $wpdb->esc_like( $query );
+
+			// Include postmeta in search.
+			foreach ( $keys as $key ) {
+				$where .= " OR {$wpdb->posts}.post_type = 'download' AND {$wpdb->posts}.ID IN (SELECT {$wpdb->postmeta}.post_id FROM {$wpdb->posts}, {$wpdb->postmeta} WHERE {$wpdb->postmeta}.meta_key = '$key' AND {$wpdb->postmeta}.meta_value LIKE '%$query%' AND {$wpdb->posts}.ID = {$wpdb->postmeta}.post_id)";
+			}
+
+			// Include taxonomy terms in search.
+			$where .= " OR {$wpdb->posts}.post_type = 'download' AND {$wpdb->posts}.ID IN (SELECT {$wpdb->posts}.ID FROM {$wpdb->posts},{$wpdb->term_relationships},{$wpdb->terms} WHERE {$wpdb->posts}.ID = {$wpdb->term_relationships}.object_id AND {$wpdb->term_relationships}.term_taxonomy_id = {$wpdb->terms}.term_id AND {$wpdb->terms}.name LIKE '%$query%')";
+
+			return $where;
+		}
+	}
+}
+
+// Kick it off.
+add_action( 'plugins_loaded', array( wds_edd_search(), 'hooks' ) );
